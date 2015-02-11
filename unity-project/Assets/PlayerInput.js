42e2406ea58aa0970f88205d897a93a0db4939cb
Aaron Morris <aaorris@live.com>
2015-02-11 12:43:15 -0500

    Adding bird rig. Freaky.

--- unity-project/Assets/PlayerInput.js
+++ unity-project/Assets/PlayerInput.js
@@ -9,6 +9,10 @@ function Update () {
         rigidbody.AddForce (Vector3.up * 100);
     if(Input.GetKey("down"))
         rigidbody.AddForce (Vector3.down * 100);
+    if(Input.GetKey("left"))
+      transform.Rotate(0,-1.5,0);
+    if(Input.GetKey("right"))
+    transform.Rotate(0,1.5,0);
     if(Input.GetKey("w"))
         rigidbody.AddRelativeForce (Vector3.forward * 100);
     if(Input.GetKey("s"))
