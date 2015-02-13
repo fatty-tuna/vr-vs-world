d42089305e0359ab7c068d09d2b87711198a8661
Aaron Morris <aaorris@live.com>
2015-02-12 22:15:07 -0500

    Basic AI
    solves #19

--- unity-project/Assets/Charge.js
+++ unity-project/Assets/Charge.js
@@ -1,22 +1,25 @@
 #pragma strict
 
+var tracking: DetectPresence;
+
 function Start () {
 }
 
 function Update () {
-    var target = GameObject.Find("Player");
     var speed = 5*Time.deltaTime;
-    transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed);
-    transform.LookAt(target.transform.position);
+    if(tracking.target) {
+      transform.position = Vector3.MoveTowards(transform.position, tracking.target.transform.position, speed);
+      transform.LookAt(tracking.target.transform.position);
+    }
 }
 
 function OnCollisionEnter(collision : Collision) {
     // Debug-draw all contact points and normals
-    for (var contact : ContactPoint in collision.contacts) {
-        Debug.DrawRay(contact.point, contact.normal, Color.white);
-    }
+    // for (var contact : ContactPoint in collision.contacts) {
+    //     Debug.DrawRay(contact.point, contact.normal, Color.white);
+    // }
 
-    // Play a sound if the coliding objects had a big impact.		
-    if (collision.relativeVelocity.magnitude > 2)
-        audio.Play();
+    // Play a sound if the coliding objects had a big impact.
+    // if (collision.relativeVelocity.magnitude > 2)
+    //     audio.Play();
 }
---
+++ unity-project/Assets/DetectPresence.js
@@ -0,0 +1,39 @@
+﻿#pragma strict
+
+var player : GameObject;
+var target : Transform;
+var lookAngle = 0;
+var hasTarget=true;
+
+function Start () {
+
+}
+
+function Update () {
+  if(!hasTarget) {
+    transform.Rotate(Vector3.up * Time.deltaTime * Mathf.Sin(Time.time/1000.0), Space.World);
+  }
+}
+
+function OnTriggerEnter(other: Collider) {
+  if(other.gameObject == player) {
+    print("Found " + other.gameObject);
+    hasTarget=true;
+  }
+}
+
+function OnTriggerExit(other: Collider) {
+  if(other.gameObject == player) {
+    print("Lost " + other.gameObject);
+    target = null;
+    hasTarget=false;
+  }
+}
+
+function OnTriggerStay(other: Collider) {
+  if(other.gameObject == player) {
+    target = other.gameObject.transform;
+    print(target);
+    transform.LookAt(target);
+  }
+}
