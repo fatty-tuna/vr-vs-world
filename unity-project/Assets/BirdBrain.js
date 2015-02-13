171be84d86dd92f7d922d1fe1b5e6ae515db4726
Aaron Morris <aaorris@live.com>
2015-02-13 12:19:31 -0500

    VR integration, AI, etc

---
+++ unity-project/Assets/BirdBrain.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var animator : Animator;
+var tracking : DetectPresence;
+
+function Start () {
+  animator = gameObject.GetComponent(Animator);
+  animator.Play("idle");
+}
+
+function Update () {
+  if(tracking.target != null) {
+    animator.Play("Take 001");
+  }
+}
--- unity-project/Assets/PlayerInput.js
+++ unity-project/Assets/PlayerInput.js
@@ -5,6 +5,7 @@ function Start () {
 }
 
 function Update () {
+    //rigidbody.velocity =
     if(Input.GetKey("up"))
         rigidbody.AddForce (Vector3.up * 100);
     if(Input.GetKey("down"))
@@ -13,12 +14,26 @@ function Update () {
       transform.Rotate(0,-1.5,0);
     if(Input.GetKey("right"))
     transform.Rotate(0,1.5,0);
+
+
+    transform.Rotate(Vector3(0,-1.5,0) * Input.GetAxis("Mouse X"));
+    transform.Rotate(Vector3(0,1.5,0) * Input.GetAxis("Horizontal"));
+
+    rigidbody.velocity *= 0.9;
+
     if(Input.GetKey("w"))
         rigidbody.AddRelativeForce (Vector3.forward * 100);
+    rigidbody.AddRelativeForce (Vector3.forward*Input.GetAxis("Vertical")*100);
     if(Input.GetKey("s"))
         rigidbody.AddRelativeForce (Vector3.forward * -100);
     if(Input.GetKey("a"))
         rigidbody.AddRelativeForce (Vector3.left * 100);
     if(Input.GetKey("d"))
         rigidbody.AddRelativeForce (Vector3.right * 100);
+
+    // var speed = 5*Time.deltaTime;
+    // if(tracking.target) {
+    //   transform.position = Vector3.MoveTowards(transform.position, tracking.target.transform.position, speed);
+    //   transform.LookAt(tracking.target.transform.position);
+    // }
 }
