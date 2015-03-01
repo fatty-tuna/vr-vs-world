62f7248dac882583bb586664f44ba5c6d3807e25
Aaron Morris <aaorris@live.com>
2015-02-28 23:51:14 -0500

    Unity templates for @TheWarmFuzzy

---
+++ contributions/00_Active_Contributions/Jeffrey/MazeBlock.js
@@ -0,0 +1,85 @@
+public class MazeBlock {
+
+	public var sides = {"UP":0,"RIGHT":0,"DOWN":0,"LEFT":0};
+	public var size : int;
+
+	/*
+	@inputs
+	@outputs
+	@use
+	@TODO: null
+	*/
+	public function MazeBlock() {
+
+	}
+	/*
+	@inputs
+	@outputs
+	@use
+	@TODO: null
+	*/
+	public function CreateBlock() {
+
+	}
+
+	/*
+	@inputs
+	@outputs
+	@use
+	@TODO: null
+	*/
+	public function FixWalls(sur) {
+
+	}
+
+	/*
+	@inputs
+	@outputs
+	@use
+	@TODO: null
+	*/
+	public function FillBlock(sur) {
+
+	}
+
+	/*
+	@inputs
+	@outputs
+	@use
+	@TODO: null
+	*/
+	public function SetSides(up,right,down,left) {
+
+	}
+
+	/*
+	@inputs
+	@outputs
+	@use
+	@TODO: null
+	*/
+	public function GetValue() {
+
+	}
+
+	/*
+	@inputs
+	@outputs
+	@use
+	@TODO: null
+	*/
+	public function GetColour() {
+
+	}
+
+	/*
+	@inputs
+	@outputs
+	@use
+	@TODO: null
+	*/
+	public function GetImage() {
+
+	}
+
+}
---
+++ contributions/00_Active_Contributions/Jeffrey/MazeGenerator.js
@@ -0,0 +1,75 @@
+public class MazeGenerator extends MonoBehaviour {
+
+	var size : int;
+	var pieces : GameObject[];
+	/*
+	@inputs
+	@outputs
+	@use
+	@TODO: null
+	*/
+	public function MazeGenerator() {
+
+	}
+
+	/*
+	@inputs
+	@outputs
+	@use
+	@TODO: null
+	*/
+	public function GenerateMaze() {
+
+	}
+
+	/*
+	@inputs
+	@outputs
+	@use
+	@TODO: null
+	*/
+	public function MergeRooms() {
+
+	}
+
+	/*
+	@inputs
+	@outputs
+	@use
+	@TODO: null
+	*/
+	public function RemoveUnmarked() {
+
+	}
+
+	/*
+	@inputs
+	@outputs
+	@use
+	@TODO: null
+	*/
+	public function GetSurroundingBlocks() {
+
+	}
+
+	/*
+	@inputs
+	@outputs
+	@use
+	@TODO: null
+	*/
+	public function GetImage() {
+
+	}
+
+	/*
+	@inputs
+	@outputs
+	@use
+	@TODO: null
+	*/
+	public function GetHumanImage() {
+
+	}
+
+}
