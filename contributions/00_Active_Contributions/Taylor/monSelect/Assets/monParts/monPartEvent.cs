2e6967f2838eb7e750d893c1e3730496f5139eac
Tay Goes Ow <tay.thunderplant@live.ca>
2015-03-05 19:04:19 -0500

    Monster Part Selection
    - Basic button layout where key events dictate which part is being
    selected for the monster based on time limit
    - Code needs to be organized better for actual model implementation but
    basic structure exists
    - Buttons need to know when they are being selected: syntax to emulate
    button hover should exist

---
+++ contributions/00_Active_Contributions/Taylor/monSelect/Assets/monParts/monPartEvent.cs
@@ -0,0 +1,168 @@
+﻿//NOTE: THIS CODE SHOULD BE ONLY LIVE DURING THE TIMER PHASE
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class monPartEvent : MonoBehaviour
+{
+	//Counters
+	int currentSprite = 0;
+	int currentBox = 0;
+
+	//Button Names
+	string buttonHead = "ButtonH";
+	string buttonArm = "ButtonA";
+	string buttonLeg = "ButtonL";
+
+	//Folder Names
+	string resourceHead = "head";
+	string resourceArm = "arm";
+	string resourceLeg = "leg";
+
+	//Sprite Container
+	Sprite[] bgH, bgA, bgL;
+
+	private timer _timer;
+		
+	//Reference the three subfolders in /Asset/monParts/Resources
+	void Awake()	{
+		bgH = Resources.LoadAll<Sprite> (resourceHead);
+		bgA = Resources.LoadAll<Sprite> (resourceArm);
+		bgL = Resources.LoadAll<Sprite> (resourceLeg);
+	}
+
+	// Initialization
+	void Start () {
+		_timer = GetComponentInChildren<timer>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (_timer.timeRemaining > 0) {
+			keyUpEvent();
+			keyDownEvent ();
+			keyLeftRightEvent();
+		}
+	}
+	
+	//BASED ON WHICH BOX, WHICH PART IS BEING MANIPULATED UPWARDS
+	public void keyUpEvent () {
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			//HEAD: GOING UPWARDS
+			if (currentBox == 0) 
+				if (currentSprite == 0) {
+					GameObject.Find (buttonHead).GetComponent<Image> ().sprite = bgH [currentSprite];
+					currentSprite++;
+				}
+				else if (currentSprite == 1)	{
+					GameObject.Find (buttonHead).GetComponent<Image> ().sprite = bgH [currentSprite];
+					currentSprite++;
+				}
+				else if (currentSprite == 2) 	{
+					GameObject.Find (buttonHead).GetComponent<Image> ().sprite = bgH [currentSprite];
+					currentSprite = 0;
+				}
+
+			//ARM: GOING UPWARDS
+			if (currentBox == 1) 
+			if (currentSprite == 0) {
+				GameObject.Find (buttonArm).GetComponent<Image> ().sprite = bgA [currentSprite];
+				currentSprite++;
+			}
+			else if (currentSprite == 1)	{
+				GameObject.Find (buttonArm).GetComponent<Image> ().sprite = bgA [currentSprite];
+				currentSprite++;
+			}
+			else if (currentSprite == 2) 	{
+				GameObject.Find (buttonArm).GetComponent<Image> ().sprite = bgA [currentSprite];
+				currentSprite = 0;
+			}
+
+			//LEG: GOING UPWARDS
+			if (currentBox == 2) 
+			if (currentSprite == 0) {
+				GameObject.Find (buttonLeg).GetComponent<Image> ().sprite = bgL [currentSprite];
+				currentSprite++;
+			}
+			else if (currentSprite == 1)	{
+				GameObject.Find (buttonLeg).GetComponent<Image> ().sprite = bgL [currentSprite];
+				currentSprite++;
+			}
+			else if (currentSprite == 2) 	{
+				GameObject.Find (buttonLeg).GetComponent<Image> ().sprite = bgL [currentSprite];
+				currentSprite = 0;
+			}
+		}
+	}
+
+	//BASED ON WHICH BOX, WHICH PART IS BEING MANIPULATED DOWNWARDS
+	public void keyDownEvent () {
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			//HEAD: GOING DOWNWARDS
+			if (currentBox == 0) 
+			if (currentSprite == 2) {
+				GameObject.Find (buttonHead).GetComponent<Image> ().sprite = bgH [currentSprite];
+				currentSprite--;
+			}
+			else if (currentSprite == 1)	{
+				GameObject.Find (buttonHead).GetComponent<Image> ().sprite = bgH [currentSprite];
+				currentSprite--;
+			}
+			else if (currentSprite == 0) 	{
+				GameObject.Find (buttonHead).GetComponent<Image> ().sprite = bgH [currentSprite];
+				currentSprite = 2;
+			}
+
+			//ARM: GOING DOWNWARDS
+			if (currentBox == 1) 
+			if (currentSprite == 2) {
+				GameObject.Find (buttonArm).GetComponent<Image> ().sprite = bgA [currentSprite];
+				currentSprite--;
+			}
+			else if (currentSprite == 1)	{
+				GameObject.Find (buttonArm).GetComponent<Image> ().sprite = bgA [currentSprite];
+				currentSprite--;
+			}
+			else if (currentSprite == 0) 	{
+				GameObject.Find (buttonArm).GetComponent<Image> ().sprite = bgA [currentSprite];
+				currentSprite = 2;
+			}
+
+			//LEG: GOING DOWNWARDS
+			if (currentBox == 2) 
+			if (currentSprite == 2) {
+				GameObject.Find (buttonLeg).GetComponent<Image> ().sprite = bgL [currentSprite];
+				currentSprite--;
+			}
+			else if (currentSprite == 1)	{
+				GameObject.Find (buttonLeg).GetComponent<Image> ().sprite = bgL [currentSprite];
+				currentSprite--;
+			}
+			else if (currentSprite == 0) 	{
+				GameObject.Find (buttonLeg).GetComponent<Image> ().sprite = bgL [currentSprite];
+				currentSprite = 2;
+			}
+		}
+	}
+
+	//CHECK WHICH BOX THE USER IS GONNA BE DURING KEY EVENT
+	public void keyLeftRightEvent () {
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			if (currentBox == 0) 
+				currentBox++;
+			else if (currentBox == 1) 
+				currentBox++;
+			else if (currentBox == 2) 
+				currentBox = 0;
+		}	
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			if (currentBox == 2) 
+				currentBox--;
+			else if (currentBox == 1) 
+				currentBox--;
+			else if (currentBox == 0) 
+				currentBox = 2;
+		}
+	}
+}
---
+++ contributions/00_Active_Contributions/Taylor/monSelect/Assets/monParts/timer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class timer : MonoBehaviour {
+	public Rect position = new Rect (Screen.width/2, 10, 200, 100);
+	public GUIStyle timerStyle = null;
+	public float timeRemaining = 20;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeRemaining -= Time.deltaTime;
+
+		if (timeRemaining <= 0) { //Keep time at zero upon countdown
+			timeRemaining = 0; 
+			//Add additional features to start the Rift portion of the game
+		}
+	}
+
+	void OnGUI() {
+		//Stylize font for label
+		/*GUIStyle timerStyle = new GUIStyle (GUI.skin.label);
+		timerStyle.fontSize = 20;
+
+		Font gameFont = (Font)Resources.Load ("UPHEAVTT", typeof(Font));
+		timerStyle.font = gameFont;*/
+
+		GUI.Label (position, "Time Remaining : " + (int) timeRemaining, timerStyle);
+	}
+}
