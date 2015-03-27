08efe4bcaa67bcd5c637bd0b4c7fbaa0f93bbe5d
Aaron Morris <aaorris@live.com>
2015-03-27 14:57:59 -0400

    My contributions

---
+++ contributions/01_Week_9_Contributions/Aaron Morris/MonsterSpawner.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+var spawnLocation: GameObject;
+var spawnMonster: GameObject;
+
+function Start () {
+	Debug.Log("Starting");
+}
+
+function Update () {
+
+}
+function OnTriggerEnter(collision : Collider) {
+	Debug.Log("Spawning monster");
+	var monster = Instantiate(spawnMonster, spawnLocation.transform.position, spawnLocation.transform.rotation);
+}
