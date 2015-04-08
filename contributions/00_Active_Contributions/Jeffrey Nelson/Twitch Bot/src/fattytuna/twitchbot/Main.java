e0bba999352d1a1d77180832de9a79a1ff12a2a5
Jeffrey Nelson <asgardolympian@gmail.com>
2015-04-08 10:59:54 -0400

    May Fatty Tuna have mercy on your soul

---
+++ contributions/00_Active_Contributions/Jeffrey Nelson/Twitch Bot/src/fattytuna/twitchbot/Main.java
@@ -0,0 +1,29 @@
+package fattytuna.twitchbot;
+
+import fattytuna.twitchbot.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+    	
+    	BotGUI botGui = new BotGUI();
+		botGui.setVisible(true);
+		FTPircBot bot = new FTPircBot("198.50.168.24",3128);    //Starting up the bot
+		bot.setGUI(botGui);
+        bot.setVerbose(true);        // Enable debugging output.
+        
+        // Connect to the IRC server.
+        try{
+        	botGui.println( "Connecting to twitch...");
+        	bot.connect("irc.twitch.tv", 6667, "oauth:uolwms2n3i4cm1zlwbwr6wdyfo7epa");
+        	botGui.println( "Connected");
+        	// Join the #pircbot channel.
+            bot.joinChannel("#ds3fattytuna");
+            botGui.println("Joined channel");
+            
+        }catch(Exception e){
+        	botGui.println(e);
+        	botGui.println("E-Robot - 'EMERGENCY ROBOT REPORTING FOR DUTY!'");
+        	bot.startERobot();
+        }
+	}
+}
